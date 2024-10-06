5f05dee8cca56a5568a57d78d71ae9e38fb17bac
colinms4 <zorbology@gmail.com>
2024-10-06 12:50:30 -0700

    implemented reactboostrap for ui improvements

---
+++ src/components/login-view/login-view.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
+export const LoginView = ({ onLoggedIn }) => {
+    const [ username, setUsername ] = useState("");
+    const [ password, setPassword ] = useState("");
+    const handleSubmit = (event) => {
+    // this prevents the default behavior of the form which is to reload the entire page
+    event.preventDefault();
+
+    const data = {
+        Username: username,
+        Password: password
+      };
+  
+      fetch("https://myflixdb-movies123-5a87d32f5f6f.herokuapp.com/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Login response: ", data);
+          if (data.user) {
+            localStorage.setItem("user", JSON.stringify(data.user));
+            localStorage.setItem("token", data.token);
+            onLoggedIn(data.user, data.token);
+          } else {
+            alert("No such user");
+          }
+        })
+        .catch((e) => {
+          alert("Something went wrong");
+        });
+    }
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username:</Form.Label>
+        <Form.Control type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        required 
+        minLength={3} />
+      </Form.Group>
+      <Form.Group controlId="formPassword">
+       <Form.Label>Password:</Form.Label>
+        <Form.Control type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required />
+      </Form.Group>
+      <Button type="submit" variant="primary">
+        Submit
+      </Button>
+    </Form>
+  );
+};
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,14 +1,25 @@
-import { useEffect, useState } from "react";
-import { MovieCard } from "../movie-card";
-import { MovieView } from "../movie-view";
+import { useEffect, useState, useSyncExternalStore } from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
+import { Button, Col, Row } from "react-bootstrap";
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
     const [movies, setMovies] = useState([]);
-
     const [selectedMovie, setSelectedMovie] = useState(null);
+    const [ user, setUser ] = useState(storedUser? storedUser : null);
+    const [ token, setToken ] = useState(storedToken? storedToken : null);
 
     useEffect(() => {
-        fetch('https://myflixdb-movies123-5a87d32f5f6f.herokuapp.com/movies')
+        if (!token) {
+            return;
+        }
+        fetch('https://myflixdb-movies123-5a87d32f5f6f.herokuapp.com/movies', {
+            headers: { Authorization: `bearer ${token}`}
+        })
             .then((response) => response.json())
             .then((data) => {
                 const moviesFromApi = data.map((movie) => {
@@ -32,30 +43,50 @@ export const MainView = () => {
                 })
                 setMovies(moviesFromApi);
             })
-    })
+    },[token]);
 
-    if (selectedMovie) {
-        return (
-            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-        );
-    }
-    if (movies.length === 0) {
-        return <div>The List is empty!</div>;
-    } else {
-        return (
-            <div>
-                {movies.map((movie) => {
-                    return (
-                        <MovieCard
-                            key={movie.id}
-                            movie={movie}
-                            onMovieClick={(newSelectedMovie) => {
-                                setSelectedMovie(newSelectedMovie);
-                            }}
-                        />
-                    );
-                })}
-            </div>
-        );
-    }
+    return (
+        <Row className="justify-content-md-center">
+            {!user ? (
+                <>
+                <Col md={5}>
+                <LoginView onLoggedIn={(user) => setUser(user)} />
+                or
+                <SignupView />
+                </Col>
+                </>
+            ) : selectedMovie ? (
+                <Col md={8}>
+                <MovieView movie={selectedMovie}
+                onBackClick={() => setSelectedMovie(null)}
+                />
+                </Col>
+            ) : movies.length === 0 ? (
+                <div>The list is empty</div>
+            ) : (
+                <>
+                {movies.map((movie) => (
+                    <Col className="mb-5" key={movie.id} md={3}>
+                    <MovieCard
+                    key={movie.id}
+                    movie={movie}
+                    onMovieClick={(newSelectedMovie) => {
+                        setSelectedMovie(newSelectedMovie);
+                    }}
+                    />
+                    </Col>
+                    ))}
+                </>
+            )}
+    { user && (
+        <Col md={{ span: 4, offset: 3}}>
+        <Button onClick={() => {
+            setUser(null);
+            setToken(null);
+            localStorage.clear();
+        }} variant="primary">Logout</Button>
+        </Col>
+        )}
+        </Row>
+    );
 };
